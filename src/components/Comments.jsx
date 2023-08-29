2606f8be141f50677c0fb0eb44a0b135938325da
Cesar Pizarra <cpizarra002@gmail.com>
2023-08-29 22:15:38 +0800

    add comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -7,8 +7,8 @@ import Quote from "../assets/bg-quotes.png";
 const Comments = () => {
   return (
     <section className="w-full bg-dark-blue-main">
-      <div className="max-w-[1240px] mx-auto px-12">
-        <div className="grid grid-cols-1 md:grid-cols-3 py-32">
+      <div className="max-w-[1240px] mx-auto px-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 py-32 gap-8">
           <div className="px-10 py-6 bg-dark-blue-main2 rounded shadow-xl text-white relative ">
             <div className="z-10 ">
               <div className="absolute top-[-15%] left-0 z-0">
@@ -32,6 +32,50 @@ const Comments = () => {
                       Founder & CEO, Huddle
                     </p>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 py-6 bg-dark-blue-main2 rounded shadow-xl text-white ">
+            <div>
+              <p>
+                Fylo has improved our team productivity by an order of
+                magnitude. Since making the switch our team has become a
+                well-oiled collaboration machine.
+              </p>
+              <div className="flex items-center mt-4">
+                <img
+                  src={Profile2}
+                  alt="Profile 1"
+                  className="w-7 h-7 rounded-full"
+                />
+                <div className="ml-4">
+                  <p>Bruce Mckenzie</p>
+                  <p className="text-[10px] text-gray-400">
+                    Founder & CEO, Huddle
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 py-6 bg-dark-blue-main2 rounded shadow-xl text-white ">
+            <div>
+              <p>
+                Fylo has improved our team productivity by an order of
+                magnitude. Since making the switch our team has become a
+                well-oiled collaboration machine.
+              </p>
+              <div className="flex items-center mt-4">
+                <img
+                  src={Profile3}
+                  alt="Profile 1"
+                  className="w-7 h-7 rounded-full"
+                />
+                <div className="ml-4">
+                  <p>Iva Boyd</p>
+                  <p className="text-[10px] text-gray-400">
+                    Founder & CEO, Huddle
+                  </p>
                 </div>
               </div>
             </div>
